c85c01ed08c577eeee5e9e22230a489a1b1a16ec
Luis Pedraza <luispedraza@gmail.com>
2015-01-23 18:42:11 +0100

    Lesson 1

---
+++ app/src/main/java/es/luispedraza/sunshine/MainActivity.java
@@ -0,0 +1,99 @@
+package es.luispedraza.sunshine;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            // fake forecast data
+            String[] forecastArray = {
+                    "Today - Sunny - 88/34",
+                    "Tomorrow - Foggy - 70/40",
+                    "Weds - Cloudy - 89-30",
+                    "Thurs - Sunny - 88/34",
+                    "Fri - Foggy - 70/40",
+                    "Sat - Foggy - 89-30",
+                    "Sun - Cloudy - 88/34",
+                    "Mon - Cloudy - 70/40",
+                    "Tue - Sunny - 89-30",
+                    "Wed - Sunny - 88/34",
+                    "Thurs - Sunny - 70/40",
+                    "Fri - Cloudy - 89-30"
+            };
+            ArrayList<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
+
+            // Create the data adapter: ArrayAdapter
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getBaseContext(),
+                    R.layout.list_item_forecast,
+                    R.id.list_item_forecast_textview,
+                    weekForecast);
+            // Now, bind the adapter to the View:
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(adapter);
+
+
+            return rootView;
+        }
+    }
+}
